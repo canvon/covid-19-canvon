af0c2399408fd99e8ac64d0d0de29e7ffa699081
Fabian Pietsch <fabian@canvon.de>
2020-03-17 02:31:07 +0100

    OWID: converter v2.0: Make column delimiter variable

--- OurWorldInData.org/convert_full_data.pl
+++ OurWorldInData.org/convert_full_data.pl
@@ -6,7 +6,8 @@ use warnings;
 use DateTime;
 
 defined($_ = <>) or die("Head line missing: $!\n");
-s/,/ /g;
+my $delim = ' ';
+s/,/$delim/g;
 s/_([a-z])/\U$1/g;
 print;
 
@@ -18,7 +19,7 @@ while (<>) {
 	my $date = DateTime->new(year => $1, month => $2, day => $3);
 	my $date_out = $date->epoch;
 
-	print($date_out . " " . '"'.$location.'"' . " " . join(' ', @data) . "\n");
+	print($date_out . $delim . '"'.$location.'"' . $delim . join($delim, @data) . "\n");
 }
 
 exit(0);
