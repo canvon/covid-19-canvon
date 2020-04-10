f9a945e125606dbc922ff9b305a939b399c929fb
Fabian Pietsch <fabian@canvon.de>
2020-04-10 22:35:44 +0200

    Build system: Fix make plot-examples
    * Interpolate into `make examples/foo.png` via shell instead of via make
      itself.
    * Remove explicit quote escaping from the config.mk value,
      so that `make plot-examples` which always interpolated via shell
      will work again.

--- common/include/config.mk
+++ common/include/config.mk
@@ -3,4 +3,4 @@ export TIMESTAMP := $(shell date +%Y%m%d_%H%M%S)
 # On my system, according to fontconfig, Arial maps to Liberation Sans,
 # whereas Sans maps to DejaVu which seems to be a very bad default, here.
 # (Every character is too wide, texts won't fit where they belong, ...)
-export C19C_GNUPLOT_TERMINAL := pngcairo font \"Arial,12\"
+export C19C_GNUPLOT_TERMINAL := pngcairo font "Arial,12"
