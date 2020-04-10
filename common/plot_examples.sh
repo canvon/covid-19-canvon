ef93f6ccbe221d23c914cf60177abb0f6681684e
Fabian Pietsch <fabian@canvon.de>
2020-04-10 15:02:44 +0200

    Refactor: Introduce common/, and move include/ to it
    The intention is to later have more (data source specific)
    include/ directories, with a distinction of `include/`
    vs `../include/` too fine to handle; now it's `../common/include/`,
    instead.
    
    * plot_examples.sh:
      * Move from include/ to common/ directly, instead, as it is
        not included but run.
      * Use a helper variable for the target file name/path, while
        having to touch the use of `include/terminal.gnuplot`, anyway.
    
    * Use even more `.gnuplot` file name extensions (instead of
      `.gnuplot.inc.txt`, the `.inc` part of which is not necessary
      in explicit include/ directories), while we're having to touch
      all gnuplot instruction files, anyway.
    
    * Adjust uses of `../common/include/*.gnuplot` in all examples.
    
    * Give data source wide include files a header (referring to their
      future file name, already), while we're touching them anyway.
    
    * slide template call site head/tail, slide-templated-demo:
      Adjust uses of `../common/include/slide-templated.html.tt2`, too...

--- common/plot_examples.sh
+++ common/plot_examples.sh
@@ -62,8 +62,9 @@ for I in "$@"
 do
   echo "Plotting $I ..."
   TARGET_BASENAME="$(basename "$I" .gnuplot.txt)" || die "Extracting basename failed with exit code $?"
+  TARGET_FILE="${TARGET_DIR}/${TARGET_PREFIX}${TARGET_BASENAME}${TARGET_SUFFIX}"
   gnuplot \
-    -e "call '../include/terminal.gnuplot' 'png' '${TARGET_DIR}/${TARGET_PREFIX}${TARGET_BASENAME}${TARGET_SUFFIX}'" \
+    -e "call '../common/include/terminal.gnuplot' 'png' '${TARGET_FILE}'" \
     "$I" \
     || die "gnuplot failed with exit code $?"
   echo "    '${TARGET_BASENAME}'," >>"${TARGET_SLIDE_TT2}" || die "Appending gnuplot result image to slide.html.tt2 failed"
