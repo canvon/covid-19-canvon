11d24cecf6f362cca423572197fbf9a1d85aebb6
Fabian Pietsch <fabian@canvon.de>
2020-04-10 21:19:01 +0200

    Build system: config: Switch pngcairo font to somewhat the png default
    Instead of implicitly requesting "Sans,12" which maps to DejaVu
    on my system and looks very bad, request "Arial,12" explicitly
    which gives Liberation Sans on my system and seems to not overflow
    the text areas. Still looks somewhat different than with terminal
    png...

--- common/include/config.mk
+++ common/include/config.mk
@@ -1,2 +1,6 @@
 export TIMESTAMP := $(shell date +%Y%m%d_%H%M%S)
-export C19C_GNUPLOT_TERMINAL := pngcairo
+
+# On my system, according to fontconfig, Arial maps to Liberation Sans,
+# whereas Sans maps to DejaVu which seems to be a very bad default, here.
+# (Every character is too wide, texts won't fit where they belong, ...)
+export C19C_GNUPLOT_TERMINAL := pngcairo font \"Arial,12\"
