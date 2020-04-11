a2c733b735ab3bfeb14006f7892f48aa0d50b2da
Fabian Pietsch <fabian@canvon.de>
2020-04-11 19:27:07 +0200

    Make Android/shared-storage changes configurable
    * Use common/include/config.mk to set bash, perl and cp -a flag.
    
    So, make the explicit bash/perl invocations the default
    (which might help Windows users, if there will be any...)
    But leave the cp -a flag enabled, for Android/shared-storage users
    to disable (by setting C19C_CP_ARCHIVE_FLAG to empty).

--- common/include/config.mk
+++ common/include/config.mk
@@ -4,3 +4,7 @@ export TIMESTAMP := $(shell date +%Y%m%d_%H%M%S)
 # whereas Sans maps to DejaVu which seems to be a very bad default, here.
 # (Every character is too wide, texts won't fit where they belong, ...)
 export C19C_GNUPLOT_TERMINAL := pngcairo font "Arial,12"
+
+export C19C_BASH := bash
+export C19C_PERL := perl
+export C19C_CP_ARCHIVE_FLAG := a
